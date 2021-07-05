1acb057e8351816bf84aa7ebd6033e947ebea902
Ingvvvar <2492345@gmail.com>
2021-07-05 22:55:01 +0300

    chngd

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to ReloaD.
         </p>
 
         <a
